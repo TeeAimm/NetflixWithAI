0bda0e467e5a0a5f84eee625ed6acef3a45ed9b8
TeeAimm <taniamollah15@gmail.com>
2024-10-13 22:32:09 +0530

    Router package installed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <Body />
   );
 }
 
